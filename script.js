fc98792957e5f66bf258b7a7f8ce0f083b82321f
Gordon <58379658+Gordon233@users.noreply.github.com>
2022-12-13 16:35:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function createListElement() {
+	var li = document.createElement("li");
+	var div = document.createElement("div");
+	var delButton = document.createElement("button");
+	div.classList.add("wrapper");
+	ul.appendChild(div);
+	div.append(li,delButton);
+	li.classList.add("taskClass");
+	li.appendChild(document.createTextNode(input.value));
+	input.value = "";
+	delButton.classList.add("delClass");
+	delButton.innerHTML="delete";
+}
+
+function inputLength() {
+	return input.value.length;
+}
+
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function doneTask(task){
+	if (task.target.tagName ==="LI"){
+		task.target.classList.toggle("done");
+	}
+}
+
+function deleteListElement(element){
+	if (element.target.className === "delClass"){
+		element.target.parentElement.remove();
+	}
+}
+
+function handleUIclick(element){
+	doneTask(element);
+	deleteListElement(element);
+}
+
+ul.addEventListener("click",handleUIclick);
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+
